4d43c5fd2b5bf9f6759bf498deba9a7d667dfdf7
DareIsrael <dareisrael4@gmail.com>
2023-10-26 14:46:45 -0700

    corrected

--- models/userModels.js
+++ models/userModels.js
@@ -89,13 +89,13 @@ userSchema.statics.login = async function (email, password) {
     const user = await this.findOne({email})
 
   if (!user) {
-    throw Error ('Incorrect Email')
+    throw Error ('Invalid email or password')
   }
 
 const match = await bcrypt.compare( password, user.password)
 
 if (!match) {
-    throw Error ('Incorrect password')
+    throw Error ('Invalid email or password')
 }
 
 return user
